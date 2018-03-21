436c39d5d96f0c342f0b8cae5685128cfbc65c7a
Louis Anthony <lja8@students.uwf.edu>
2018-03-20 22:31:32 -0500

    Updated spacing. Also testing if I show up as a contributer now

--- src/keimono/Main.java
+++ src/keimono/Main.java
@@ -1,12 +1,10 @@
 package keimono;
 
-
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
-
 
 public class Main extends Application {
 	@Override
@@ -22,7 +20,7 @@ public class Main extends Application {
 		}
 	}
 
-	public static void main(String[] args) { //fix this lol
+	public static void main(String[] args) {
 		launch(args);
 	}
 }
