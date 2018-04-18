a2ef640b04933dbf6166a32186279442a13d705e
Louis Anthony <lja8@students.uwf.edu>
2018-04-18 01:16:05 -0500

    Cleaned up master to allow for us to rebuild with proper packaging

---
+++ src/keimono/model/Placeholder.java
@@ -0,0 +1,7 @@
+package keimono.model;
+
+public class Placeholder {
+
+	//this package will contain any models we use to help build the gui around
+
+}
---
+++ src/keimono/view/Placeholder.java
@@ -0,0 +1,7 @@
+package keimono.view;
+
+public class Placeholder {
+
+	//this package will contain all of the fxmls and related controllers
+
+}
